4c401d7788e1ef0704576bfd53fe9f1708737361
Leor Seligman <seligman.leor@gmail.com>
2020-01-17 19:10:48 +0200

    New Folder

--- Domain/Models/Result.cs
+++ Domain/Models/Result.cs
@@ -4,7 +4,7 @@ namespace Stream.API.Domain.Models
     {
         public int Id { get; set; }
         public double score {get;set;}
-        //Foreign Key
+        
         public int stdID {get;set;}
         public Student Student {get;set;}
    
