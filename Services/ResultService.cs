c33fa707c26206ba5b54c284f6d962464a074289
Leor Seligman <seligman.leor@gmail.com>
2020-01-17 11:08:36 +0200

    results servies created

---
+++ Services/ResultService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Stream.API.Domain.Models;
+using Stream.API.Domain.Services;
+
+namespace Stream.API.Services
+{
+    public class ResultService : IResultsService
+    {
+        public async Task<IEnumerable<Result>> ListAsync()
+        {
+            
+        }
+    }
+}
